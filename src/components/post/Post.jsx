1c4da92c0209837c91599c5515b03c425fcb194b
Senuda004 <senudamusic120@gmail.com>
2024-03-24 02:37:37 +0530

    Fix rendering issue in Post.jsx and update .gitignore

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -11,8 +11,9 @@ export default function Post({ post }) {
       {post.img && <img className="postImg" src ={PF + post.img} alt=""/>}
       <div className="postInfo">
         <div className="postCats">
-          {post.category.map(c => (
-            <span className="postCat"> {c.name} </span>
+          {/* Check if post.category is an array before using map */}
+          {Array.isArray(post.category) && post.category.map(c => (
+            <span className="postCat" key={c.name}>{c.name}</span>
           ))}
         </div>
         <Link to={`/post/${post._id}`} className="link">
